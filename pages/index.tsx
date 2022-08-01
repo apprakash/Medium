ac5be4cc3e5eb60cc007c52eae3f4e867b63032a
apprakash <jimpoorna@gmail.com>
2022-08-01 17:32:37 +0530

    header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Header from '../Components/header'
+import Header from '../Components/Header'
 
 const Home: NextPage = () => {
   return (
