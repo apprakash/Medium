71a7cd3d261f6b3f5e4aa3c7e1645795c42f9400
apprakash <jimpoorna@gmail.com>
2022-08-01 22:35:02 +0530

    hero element

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,24 @@ const Home: NextPage = () => {
       </Head>
 
       <Header/>
+
+      <div className='flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0'>
+        <div className='px-10 space-y-5'>
+          <h1 className='text-6xl max-w-xl font-serif'>
+            <span className='underline decoration-black decoration-4 '>
+              Medium 
+            </span>{" "}
+          is a place to write, read and connect. 
+          </h1>
+          <h2>
+            It's easy and free to post your thinking on any topic and connect 
+            with millions of readers. 
+          </h2>
+        </div>
+
+        <img className='hidden md:inline-flex h-32 lg:h-full' src="https://www.nicepng.com/png/detail/48-483810_medium-logo-medium-social-media-icon-png.png" 
+         alt=""/>
+      </div>
     </div>
   )
 }
