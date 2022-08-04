5c2dc22841994c42960acd02daba716b5dd8c87f
apprakash <jimpoorna@gmail.com>
2022-08-04 20:26:39 +0530

    comment section

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,14 +3,14 @@ import Header from "../../Components/Header";
 import { Post } from "../../typings";
 import { GetStaticProps } from "next";
 import PortableText from "react-portable-text";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 interface Props {
     post: Post;
 }
 
 function Post({post }: Props) {
-  console.log(post);
-
+  
   return (
     <main>
       <Header />
@@ -72,6 +72,37 @@ function Post({post }: Props) {
         </div>
 
       </article>
+
+      <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
+
+      <form className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
+
+        <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+        <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+        <hr className="py-3 mt-2" />
+
+        <label className="block mb-5">
+          <span className="text-gray-700">Name</span>
+          <input
+            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+            placeholder="Enter Name" type="text" />
+        </label>
+
+        <label className="block mb-5">
+          <span className="text-gray-700">E-mail</span>
+          <input
+            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+            placeholder="Enter address" type="text" />
+        </label>
+
+        <label className="block mb-5">
+          <span className="text-gray-700">Comment</span>
+          <textarea
+            className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
+            placeholder="Enter here" rows={8} />
+        </label>
+
+      </form>
     </main>
   ); 
 }
